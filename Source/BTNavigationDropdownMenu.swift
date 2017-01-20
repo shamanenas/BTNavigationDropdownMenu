a317dc8fcf108a7328b1719029e784999827501a
shamanenas <binisevicius@gmail.com>
2017-01-20 09:07:43 +0200

    Fix optionals

--- Source/BTNavigationDropdownMenu.swift
+++ Source/BTNavigationDropdownMenu.swift
@@ -465,8 +465,8 @@ public class BTNavigationDropdownMenu: UIView {
     }
     
     open func updateMenuTitle(title: String) {
-        self?.setMenuTitle(title)
-        self?.layoutSubviews()
+        self.setMenuTitle(title)
+        self.layoutSubviews()
     }
     
     func menuButtonTapped(sender: UIButton) {
